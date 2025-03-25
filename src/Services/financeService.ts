a5ba39712f84aba6139e5d18cce0793a66b11676
a.akbari <dev.amirhossein01@gmail.com>
2025-03-26 01:49:46 +0330

    update count in ranges

--- src/Services/financeService.ts
+++ src/Services/financeService.ts
@@ -1,6 +1,6 @@
 import { Trade } from '@prisma/client';
 import sendLog from './logger';
-import { CandleResponse, OrderBlockVM } from '../finance/types';
+import { CandleResponse, Candlestick, OrderBlockVM } from '../finance/types';
 import { calculatePercentageChange, formatData } from '../finance/util';
 import { GetByOrderBlockStrategy } from '../finance/algOrderBlock';
 // import { FastifyInstance } from 'fastify';
@@ -13,7 +13,9 @@ dotenv.config()
 export class FinanceService extends baseService {
 
     async addTrade(trade: Trade) {
-        const twoHoursAgo = new Date(Date.now() - 2 * 60 * 60 * 1000);
+        let limit = 4;
+
+        const HoursAgo = new Date(Date.now() - limit * 60 * 60 * 1000);
 
         let exist = await this.context.trade.findFirst({
             where: {
@@ -21,7 +23,7 @@ export class FinanceService extends baseService {
                     equals: trade.candleTimeStamp
                 },
                 recordDate: {
-                    gte: twoHoursAgo
+                    gte: HoursAgo
                 }
             }
         })
@@ -72,9 +74,9 @@ export class FinanceService extends baseService {
         // 1- loop through the trade opportunities.
         result.forEach(async (item: OrderBlockVM) => {
 
-            let zoneLow,
-                zoneHigh,
-                isBuy;
+            let zoneLow: number,
+                zoneHigh: number,
+                isBuy: boolean;
 
             if (item.baseHigh != null && item.baseLow != null) {
                 zoneLow = item.baseLow;
@@ -97,60 +99,115 @@ export class FinanceService extends baseService {
             // 3- if the price is bellow the zone, it should be sell signal. if the price was above, it should be buy signal
             if ((currentPrice > zoneHigh || currentPrice < zoneLow) && change <= (time == "minute" ? 0.20 : 0.70)) {// if the price was not in the zone
 
-
+                let entry: number,
+                    sl: number,
+                    tp: number,
+                    signalDesc: string;
+                // here count countCandlesInRange
+                let afterCandles = sortedList.filter(x => x.id < item.id);
+                const { closeInRangeCount, passesThroughRangeCount } = countCandlesInRange(afterCandles, zoneLow, zoneHigh)
 
                 if (isBuy) {
-                    let entry = zoneHigh
-                    let sl = zoneLow - 0.05
-                    let tp = (Math.abs(sl - entry) * 3) + entry
-
-                    //@ts-ignore
-                    let newTrade: Trade = {
-                        pair: symbol,
-                        algoritm: 'order block',
-                        tp: tp,
-                        sl: sl,
-                        candleTime: item.time,
-                        candleInfo: JSON.stringify(item),
-                        volume: item.volume,
-                        candleTimeStamp: String(sortedList[0].timestamp),
-                        orderType: EOrderType.BUY,
-                        status: EStatus.PENDING,
-                        timeFrame: timeFrame
-                    }
-                    let addRes = await this.addTrade(newTrade)
-                    if (addRes)
-                        await sendLog("Signal", `🟢 buy signal on ${symbol.toUpperCase()}\nTimeFrame: ${timeFrame}\nEntry: ${entry}\nSL: ${sl}\nTP: ${tp}\nzoneHigh: ${zoneHigh}\nzoneLow: ${zoneLow}\nvolume: ${item.volume} \n\n #${symbol} #${timeFrame} #${symbol}${timeFrame}`, item.time)
+                    entry = zoneHigh
+                    sl = zoneLow - 0.05
+                    tp = (Math.abs(sl - entry) * 3) + entry
+                    signalDesc = '🟢 buy signal on ';
+                    // //@ts-ignore
+                    // let newTrade: Trade = {
+                    //     pair: symbol,
+                    //     algoritm: 'order block',
+                    //     tp: tp,
+                    //     sl: sl,
+                    //     candleTime: item.time,
+                    //     candleInfo: JSON.stringify(item),
+                    //     volume: item.volume,
+                    //     candleTimeStamp: String(sortedList[0].timestamp),
+                    //     orderType: EOrderType.BUY,
+                    //     status: EStatus.PENDING,
+                    //     timeFrame: timeFrame
+                    // }
+                    // let addRes = await this.addTrade(newTrade)
+                    // if (addRes)
+                    //     await sendLog("Signal", `🟢 buy signal on ${symbol.toUpperCase()}\nTimeFrame: ${timeFrame}\nEntry: ${entry}\nSL: ${sl}\nTP: ${tp}\nzoneHigh: ${zoneHigh}\nzoneLow: ${zoneLow}\nvolume: ${item.volume} \n\n #${symbol} #${timeFrame} #${symbol}${timeFrame}`, item.time)
                 } else {
-                    let entry = zoneLow
-                    let sl = zoneHigh + 0.05
-                    let tp = entry - (Math.abs(sl - entry) * 3)
+                    entry = zoneLow
+                    sl = zoneHigh + 0.05
+                    tp = entry - (Math.abs(sl - entry) * 3)
+                    signalDesc = '🔴 Sell signal on ';
 
-                    //@ts-ignore
-                    let newTrade: Trade = {
-                        pair: symbol,
-                        algoritm: 'order block',
-                        tp: tp,
-                        sl: sl,
-                        candleTime: item.time,
-                        volume: item.volume,
-                        candleInfo: JSON.stringify(item),
-                        candleTimeStamp: String(sortedList[0].timestamp),
-                        orderType: EOrderType.SELL,
-                        status: EStatus.PENDING,
-                        timeFrame: timeFrame
-                    }
-                    let addRes = await this.addTrade(newTrade)
+                    // //@ts-ignore
+                    // let newTrade: Trade = {
+                    //     pair: symbol,
+                    //     algoritm: 'order block',
+                    //     tp: tp,
+                    //     sl: sl,
+                    //     candleTime: item.time,
+                    //     volume: item.volume,
+                    //     candleInfo: JSON.stringify(item),
+                    //     candleTimeStamp: String(sortedList[0].timestamp),
+                    //     orderType: EOrderType.SELL,
+                    //     status: EStatus.PENDING,
+                    //     timeFrame: timeFrame
+                    // }
+                    // let addRes = await this.addTrade(newTrade)
 
-                    if (addRes)
-                        await sendLog("Signal", `🔴 Sell signal on ${symbol.toUpperCase()}\nTimeFrame: ${timeFrame}\nEntry: ${entry}\nSL: ${sl}\nTP: ${tp}\nzoneHigh: ${zoneHigh}\nzoneLow: ${zoneLow}\nvolume: ${item.volume} \n\n #${symbol} #${timeFrame} #${symbol}${timeFrame}`, item.time)
+                    // if (addRes)
+                    //     await sendLog("Signal", `🔴 Sell signal on ${symbol.toUpperCase()}\nTimeFrame: ${timeFrame}\nEntry: ${entry}\nSL: ${sl}\nTP: ${tp}\nzoneHigh: ${zoneHigh}\nzoneLow: ${zoneLow}\nvolume: ${item.volume} \n\n #${symbol} #${timeFrame} #${symbol}${timeFrame}`, item.time)
 
                 }
+
+
+
+                //@ts-ignore
+                let newTrade: Trade = {
+                    pair: symbol,
+                    algoritm: 'order block',
+                    tp: tp,
+                    sl: sl,
+                    candleTime: item.time,
+                    candleInfo: JSON.stringify(item),
+                    volume: item.volume,
+                    candleTimeStamp: String(sortedList[0].timestamp),
+                    orderType: EOrderType.BUY,
+                    status: EStatus.PENDING,
+                    timeFrame: timeFrame
+                }
+
+
+                let addRes = await this.addTrade(newTrade)
+
+                if (addRes)
+                    await sendLog("Signal", `${signalDesc} ${symbol.toUpperCase()}\nTimeFrame: ${timeFrame}\nEntry: ${entry}\nSL: ${sl}\nTP: ${tp}\n-------------------------\n\nzoneHigh: ${zoneHigh}\nzoneLow: ${zoneLow}\nvolume: ${item.volume}\nclosedInCount: ${closeInRangeCount}\nPassesThroughCount: ${passesThroughRangeCount} \n\n #${symbol} #${timeFrame} #${symbol}${timeFrame}`, item.time)
 
             }
         })
 
     }
+}
+
+function countCandlesInRange(candles: Candlestick[], minRange: number, maxRange: number) {
+    let closeInRangeCount = 0;
+    let passesThroughRangeCount = 0;
+
+    for (const candle of candles) {
+        const { open, close } = candle;
+
+        // Check if close is within the range
+        if (close >= minRange && close <= maxRange) {
+            closeInRangeCount++;
+        }
+
+        // Check if candle passes through the range but both open and close are outside
+        if ((open < minRange && close > maxRange) ||  // Opens below and closes above
+            (open > maxRange && close < minRange)) {  // Opens above and closes below
+            passesThroughRangeCount++;
+        }
+    }
+
+    return {
+        closeInRangeCount,
+        passesThroughRangeCount
+    };
 }
 
 
